a06c5743f048519cde632c7dd2660af1f237017e
wgerrish-eng <wgerrish@trinitypawling.org>
2026-02-17 15:09:28 -0500

    wwwww

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy as np
 #from tmdb3 import_set_key
 import requests
-#set_key('')
+import json
+import time
 
 movie = pd.read_csv('file/movies.csv')
 tag = pd.read_csv('file/tags.csv')
@@ -17,16 +18,26 @@ movie_id = pd.merge(
 )
 movie_id.to_csv('player_full.csv', index=False)
 
+for i in range (2,1186337):
+    try:
+        with open("movies.json", "r") as jsonfile:
+            data = json.load(jsonfile)
+        time.sleep(0.5)
+        url = "https://api.themoviedb.org/3/movie/"+str(i)+"?language=en-US"
 
-url = "https://api.themoviedb.org/3/movie/862?language=en-US"
-
-headers = {
-    "accept": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmODhmZmUyOTZlZTE3OGM5NGUzNDAxNTEwOTQ0YjIxMCIsIm5iZiI6MTc3MTI1NDg0NS4zNjQsInN1YiI6IjY5OTMzNDNkNGRlM2E3ZjYyNGZhMzY2ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xo7FytcVInjeW5Xs0TI6rYf3ABgdqjizBHPy8eNPIjg "
-}
+        headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmODhmZmUyOTZlZTE3OGM5NGUzNDAxNTEwOTQ0YjIxMCIsIm5iZiI6MTc3MTI1NDg0NS4zNjQsInN1YiI6IjY5OTMzNDNkNGRlM2E3ZjYyNGZhMzY2ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xo7FytcVInjeW5Xs0TI6rYf3ABgdqjizBHPy8eNPIjg "
+        }
 
 
-response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers)
 
-print(response.text)
-movie.json = response.text
+        print(response.text)
+        data.append(response.text)
+
+        with open("movies.json", "w") as jsonfile:
+            json.dump(data, jsonfile)
+
+    except Exception as e:
+        print(e)
