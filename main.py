acea245e91a163e06d6e4db386a92a412c72831b
wgerrish-eng <wgerrish@trinitypawling.org>
2026-02-17 14:35:57 -0500

    code

--- main.py
+++ main.py
@@ -21,11 +21,12 @@ movie_id.to_csv('player_full.csv', index=False)
 url = "https://api.themoviedb.org/3/movie/862?language=en-US"
 
 headers = {
-    "accept": "application/json"
-
+    "accept": "application/json",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmODhmZmUyOTZlZTE3OGM5NGUzNDAxNTEwOTQ0YjIxMCIsIm5iZiI6MTc3MTI1NDg0NS4zNjQsInN1YiI6IjY5OTMzNDNkNGRlM2E3ZjYyNGZhMzY2ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xo7FytcVInjeW5Xs0TI6rYf3ABgdqjizBHPy8eNPIjg "
 }
 
 
 response = requests.get(url, headers=headers)
 
 print(response.text)
+movie.json = response.text
