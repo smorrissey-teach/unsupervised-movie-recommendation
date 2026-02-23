3e764919f7442c0e341673c147707561f9d8ee31
Beaver622 <160510648+Beaver622@users.noreply.github.com>
2026-02-23 11:28:50 -0500

    load and read Movie_json

--- main.py
+++ main.py
@@ -5,39 +5,68 @@ import requests
 import json
 import time
 
-movie = pd.read_csv('file/movies.csv')
-tag = pd.read_csv('file/tags.csv')
-rating = pd.read_csv('file/ratings.csv')
-link =pd.read_csv('file/links.csv')
+# movie = pd.read_csv('movies.csv')
+# tag = pd.read_csv('tags.csv')
+# rating = pd.read_csv('ratings.csv')
+# link =pd.read_csv('links.csv')
+#
+# movie_id = pd.merge(
+#    movie,
+#     link,
+#     on='movieId',
+#     how='left'
+# )
+# movie_id.to_csv('player_full.csv', index=False)
 
-movie_id = pd.merge(
-   movie,
-    link,
-    on='movieId',
-    how='left'
-)
-movie_id.to_csv('player_full.csv', index=False)
+#for i in range (2,1186337):
+#
+# try:
+#     with open("movies.json", "r") as jsonfile:
+#         data = json.load(jsonfile)
+#     time.sleep(0.5)
+#     url = "https://api.themoviedb.org/3/movie/"+str(i)+"?language=en-US"
+#
+#     headers = {
+#     "accept": "application/json",
+#     "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmODhmZmUyOTZlZTE3OGM5NGUzNDAxNTEwOTQ0YjIxMCIsIm5iZiI6MTc3MTI1NDg0NS4zNjQsInN1YiI6IjY5OTMzNDNkNGRlM2E3ZjYyNGZhMzY2ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xo7FytcVInjeW5Xs0TI6rYf3ABgdqjizBHPy8eNPIjg "
+#     }
+#
+#
+#     response = requests.get(url, headers=headers)
+#
+#     print(response.text)
+#     data.append(response.text)
+#
+#     with open("movies.json", "w") as jsonfile:
+#         json.dump(data, jsonfile)
+#
+# except Exception as e:
+#     print(e)
+#
+#
 
-for i in range (2,1186337):
-    try:
-        with open("movies.json", "r") as jsonfile:
-            data = json.load(jsonfile)
-        time.sleep(0.5)
-        url = "https://api.themoviedb.org/3/movie/"+str(i)+"?language=en-US"
 
-        headers = {
-        "accept": "application/json",
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmODhmZmUyOTZlZTE3OGM5NGUzNDAxNTEwOTQ0YjIxMCIsIm5iZiI6MTc3MTI1NDg0NS4zNjQsInN1YiI6IjY5OTMzNDNkNGRlM2E3ZjYyNGZhMzY2ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xo7FytcVInjeW5Xs0TI6rYf3ABgdqjizBHPy8eNPIjg "
-        }
+url = "https://api.themoviedb.org/3/movie/862?language=en-US"
+
+headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmODhmZmUyOTZlZTE3OGM5NGUzNDAxNTEwOTQ0YjIxMCIsIm5iZiI6MTc3MTI1NDg0NS4zNjQsInN1YiI6IjY5OTMzNDNkNGRlM2E3ZjYyNGZhMzY2ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xo7FytcVInjeW5Xs0TI6rYf3ABgdqjizBHPy8eNPIjg "
+}
+
+response = requests.get(url, headers=headers)
+#print(response.text)
+
+# Source - https://stackoverflow.com/a/5214587
+# Posted by John La Rooy, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-02-23, License - CC BY-SA 4.0
+
+with open("response.json", "w") as text_file:
+    text_file.write(response.text)
 
 
-        response = requests.get(url, headers=headers)
+with open('response.json') as f:
+    movies_json = json.load(f)
+    movie= movies_json[0]
+    popularity = movie["popularity"]
+    print(popularity)
 
-        print(response.text)
-        data.append(response.text)
-
-        with open("movies.json", "w") as jsonfile:
-            json.dump(data, jsonfile)
-
-    except Exception as e:
-        print(e)
